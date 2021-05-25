fccf5b3597159e5b035db004d82ce898d45c4514
Tomas Holmberg <tomas.holmberg@windriver.com>
2021-05-25 07:48:25 +0000

    removed force flag to add-layer

--- scripts/pre-build.sh
+++ scripts/pre-build.sh
@@ -4,4 +4,4 @@
 set -x
 
 git clone --single-branch --branch main ${GITLAB_URL}/tholmber/studio
-bitbake-layers add-layer -F $PWD/studio/layers/th-container
+bitbake-layers add-layer $PWD/studio/layers/th-container
